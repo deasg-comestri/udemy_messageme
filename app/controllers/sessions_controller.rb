ed7592de1e230447f4d2e5c3a0cce69eb2f8d5e0
Gavin Deas <gavin.deas@comestri.com>
2020-10-21 08:30:36 +0100

    Add login/out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Logged in successfully'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Username and/or password were incorrect'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have logged out'
+    redirect_to login_path
+  end
+
 end
