f5307953c90d82243647445b7898c03e20d4f41b
Gavin Deas <gavin.deas@comestri.com>
2020-10-22 09:23:29 +0100

    Tidy up chat window styling and functioning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,6 +24,14 @@ scroll_bottom = function() {
   }
 }
 
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = '';
+    }
+  });
+}
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
@@ -31,5 +39,6 @@ $(document).on('turbolinks:load', function() {
     $(this).closest('.message').transition('fade');
   });
 
+  submit_message();
   scroll_bottom();
 })
